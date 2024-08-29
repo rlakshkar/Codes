b354b34c9cba32aa7be2ffb86f2ecc0db7106eb5
rlakshkar <lakshkar7859@gmail.com>
2024-08-29 16:56:07 +0530

    New Changes

--- Dp/ClimbingStairs.java
+++ Dp/ClimbingStairs.java
@@ -2,8 +2,8 @@ package Dp;
 
 public class ClimbingStairs {
 	public static int recursion(int n) {
-		if(n==0 || n==1 || n==2) {
-			return n;
+		if(n==0 || n==1) {
+			return 1;
 		}
 		return recursion(n-1)+recursion(n-2);
 	}
@@ -35,7 +35,7 @@ public class ClimbingStairs {
 	}
 	public static void main(String[] args) {
 		int dp[]=new int[6];
-		System.out.println(recursion(4));
+		System.out.println(recursion(5));
 		//System.out.println(memoisation(4,dp));
 		System.out.println(tabulation(5,dp));
 
