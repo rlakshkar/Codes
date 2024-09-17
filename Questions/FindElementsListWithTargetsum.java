acfb3e5b2780b7456ccf9fd4aaf67b302b1fbee9
rlakshkar <lakshkar7859@gmail.com>
2024-09-17 10:20:04 +0530

    New Changes

--- Questions/FindElementsListWithTargetsum.java
+++ Questions/FindElementsListWithTargetsum.java
@@ -9,21 +9,21 @@ public class FindElementsListWithTargetsum {
     public static void main(String[] args) {
         int array[] = { 1,0, -1, 0, -2, 2};
         int target = 0;
-        List<List<Integer>> result = new ArrayList<>();
-        findFourSum(array, target, 0, new ArrayList<>(), result);
-        System.out.println(result);
+        List<List<Integer>> resultList = new ArrayList<>();
+        findFourSum(array, target, 0, new ArrayList<>(), resultList);
+        System.out.println(resultList);
     }
 
-    public static void findFourSum(int[] array, int target, int start, List<Integer> current, List<List<Integer>> result) {
-        if (current.size() == 4) {
+    public static void findFourSum(int[] array, int target, int start, List<Integer> list, List<List<Integer>> resultList) {
+        if (list.size() == 4) {
             int sum = 0;
 
-            for(int i=0;i<current.size();i++){
-                sum+= current.get(i);
+            for(int i=0;i<list.size();i++){
+                sum+= list.get(i);
             }
 
             if (sum == target) {
-                result.add(new ArrayList<>(current));
+                resultList.add(new ArrayList<>(list));
             }
             return;
         }
@@ -33,9 +33,9 @@ public class FindElementsListWithTargetsum {
         }
 
         for (int i = start; i < array.length; i++) {
-            current.add(array[i]);
-            findFourSum(array, target, i + 1, current, result);
-            current.remove(current.size() - 1);
+            list.add(array[i]);
+            findFourSum(array, target, i + 1, list, resultList);
+            list.remove(list.size() - 1);
         }
     }
 }
