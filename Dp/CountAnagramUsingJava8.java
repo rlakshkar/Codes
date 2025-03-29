290dac14008ef8ba32285238695e342eb33abd39
RAHUL <lakshkar7859@gmail.com>
2025-03-29 11:29:32 +0530

    add change

---
+++ Dp/CountAnagramUsingJava8.java
@@ -0,0 +1,11 @@
+package Dp;
+import java.util.*;
+import java.util.stream.*;
+
+
+public class CountAnagramUsingJava8 {
+    public static void main(String[] args) {
+      String s[]={"Hello","world","heoll","hlole","dlr","dlrow"};
+      Arrays.stream(s).collect(Collectors.groupingBy(s1-> s1.chars().sorted().mapToObj(c-> String.valueOf((char) c)).collect(Collectors.joining()).toString(),Collectors.counting())).forEach((k,v)->System.out.println(k+" "+v));
+  }
+}
--- Dp/threadpool.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.util.stream.*;
-
-public class Main {
-    public static void main(String[] args) {
-      String s[]={"Hello","world","heoll","hlole","dlr","dlrow"};
-      Arrays.stream(s).collect(Collectors.groupingBy(s-> s.chars.sorted().collect(Collectors.joining()).toString(),Collectors.counting())).forEach((k,v)->System.out.println(k+" "+v));
-  }
-}
