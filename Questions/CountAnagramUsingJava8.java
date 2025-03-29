48d9c7882df332b34f5b56ad1c0ef8cdea1dc750
RAHUL <lakshkar7859@gmail.com>
2025-03-29 11:38:06 +0530

    add change

--- Questions/CountAnagramUsingJava8.java
+++ Questions/CountAnagramUsingJava8.java
@@ -6,6 +6,6 @@ import java.util.stream.*;
 public class CountAnagramUsingJava8 {
     public static void main(String[] args) {
       String s[]={"Hello","world","heoll","hlole","dlr","dlrow"};
-      Arrays.stream(s).collect(Collectors.groupingBy(s1-> s1.toLowerCase().chars().sorted().mapToObj(c-> String.valueOf((char) c)).collect(Collectors.joining()).toString(),Collectors.counting())).forEach((k,v)->System.out.println(k+" "+v));
+      Arrays.stream(s).collect(Collectors.groupingBy(s1-> s1.toLowerCase().chars().sorted().mapToObj(c-> String.valueOf((char) c)).collect(Collectors.joining()),Collectors.counting())).forEach((k,v)->System.out.println(k+" "+v));
   }
 }
